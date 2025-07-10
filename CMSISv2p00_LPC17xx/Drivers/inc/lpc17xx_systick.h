c4bc892ef16f79d3ea6695f5f80856431f8ac1dd
David <d.trujillo@unc.edu.ar>
2025-07-09 21:28:50 -0300

    lpc17xx_systick driver Clarified parameter units and masking behavior in SysTick driver documentation. Improved inline docs for reload value handling and distinguished between ms and tick-based APIs.

--- CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_systick.h
+++ CMSISv2p00_LPC17xx/Drivers/inc/lpc17xx_systick.h
@@ -31,37 +31,35 @@
 #include "LPC17xx.h"
 #include "lpc_types.h"
 
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-
 
 /* Private Macros ------------------------------------------------------------- */
 /** @defgroup SYSTICK_Private_Macros SYSTICK Private Macros
  * @{
  */
 /*********************************************************************//**
- * Macro defines for System Timer Control and status (STCTRL) register
+ * Macro defines for System Timer Control and status (STCTRL) register.
  **********************************************************************/
-#define ST_CTRL_ENABLE		((uint32_t)(1<<0))
-#define ST_CTRL_TICKINT		((uint32_t)(1<<1))
-#define ST_CTRL_CLKSOURCE	((uint32_t)(1<<2))
-#define ST_CTRL_COUNTFLAG	((uint32_t)(1<<16))
+#define ST_CTRL_ENABLE		    ((uint32_t)(1<<0))
+#define ST_CTRL_TICKINT		    ((uint32_t)(1<<1))
+#define ST_CTRL_CLKSOURCE	    ((uint32_t)(1<<2))
+#define ST_CTRL_COUNTFLAG	    ((uint32_t)(1<<16))
 
 /*********************************************************************//**
- * Macro defines for System Timer Reload value (STRELOAD) register
+ * Macro defines for System Timer Reload value (STRELOAD) register.
  **********************************************************************/
 #define ST_RELOAD_RELOAD(n)		((uint32_t)(n & 0x00FFFFFF))
 
 /*********************************************************************//**
- * Macro defines for System Timer Current value (STCURRENT) register
+ * Macro defines for System Timer Current value (STCURRENT) register.
  **********************************************************************/
 #define ST_RELOAD_CURRENT(n)	((uint32_t)(n & 0x00FFFFFF))
 
 /*********************************************************************//**
- * Macro defines for System Timer Calibration value (STCALIB) register
+ * Macro defines for System Timer Calibration value (STCALIB) register.
  **********************************************************************/
 #define ST_CALIB_TENMS(n)		((uint32_t)(n & 0x00FFFFFF))
 #define ST_CALIB_SKEW			((uint32_t)(1<<30))
@@ -70,23 +68,100 @@ extern "C"
 #define CLKSOURCE_EXT			((uint32_t)(0))
 #define CLKSOURCE_CPU			((uint32_t)(1))
 
+#define ST_MAX_LOAD             ((uint32_t)(0x00FFFFFF))
+
 /**
  * @}
  */
-
 
 /* Public Functions ----------------------------------------------------------- */
 /** @defgroup SYSTICK_Public_Functions SYSTICK Public Functions
  * @{
  */
 
+/*********************************************************************//**
+ * @brief 		Initializes the System Tick timer using the internal CPU clock source.
+ * @param[in]	time    Time interval in milliseconds.
+ * @note		If the requested time exceeds the maximum possible interval for the SysTick timer,
+ *              the LOAD register is set to its maximum value ST_MAX_LOAD, resulting in the
+ *              longest possible timer interval.
+ **********************************************************************/
 void SYSTICK_InternalInit(uint32_t time);
-void SYSTICK_ExternalInit(uint32_t freq, uint32_t time);
 
+
+/*********************************************************************//**
+ * @brief       Initializes the System Tick timer using an external clock source.
+ * @param[in]   extFreq External clock frequency in Hz.
+ * @param[in]   time    Time interval in milliseconds.
+ * @note        If the requested time exceeds the maximum possible interval for the SysTick timer
+ *              with the given external frequency, the LOAD register is set to its maximum value
+ *              ST_MAX_LOAD, resulting in the longest possible timer interval.
+ **********************************************************************/
+void SYSTICK_ExternalInit(uint32_t extFreq, uint32_t time);
+
+/*********************************************************************//**
+ * @brief       Enable or disable the System Tick counter.
+ * @param[in]   NewState    System Tick counter status, should be:
+ *                          - ENABLE
+ *                          - DISABLE
+ **********************************************************************/
 void SYSTICK_Cmd(FunctionalState NewState);
+
+/*********************************************************************//**
+ * @brief       Enable or disable the System Tick interrupt.
+ * @param[in]   NewState    System Tick interrupt status, should be:
+ *                          - ENABLE
+ *                          - DISABLE
+ **********************************************************************/
 void SYSTICK_IntCmd(FunctionalState NewState);
-uint32_t SYSTICK_GetCurrentValue(void);
-void SYSTICK_ClearCounterFlag(void);
+
+/*********************************************************************//**
+ * @brief       Get the current value of the System Tick counter.
+ * @return      Current value of the System Tick counter.
+ **********************************************************************/
+static __INLINE uint32_t SYSTICK_GetCurrentValue(void) {
+    return (SysTick->VAL);
+}
+
+/*********************************************************************//**
+ * @brief       Clear the System Tick counter flag.
+ **********************************************************************/
+static __INLINE void SYSTICK_ClearCounterFlag(void) {
+    (void)SysTick->CTRL;
+}
+
+/*********************************************************************//**
+ * @brief       Get the current reload value of the System Tick timer.
+ * @return      Current reload value.
+ **********************************************************************/
+static __INLINE uint32_t SYSTICK_GetReloadValue(void) {
+    return (SysTick->LOAD & ST_MAX_LOAD);
+}
+
+/*********************************************************************//**
+ * @brief       Set a new reload value for the System Tick timer.
+ * @param[in]   reloadTicks Reload value to set, in SysTick timer ticks.
+ * @note        If reloadValue exceeds 24 bits, only the least significant 24 bits are used.
+ **********************************************************************/
+static __INLINE void SYSTICK_SetReloadValue(uint32_t reloadTicks) {
+    SysTick->LOAD = (reloadTicks & ST_MAX_LOAD);
+}
+
+/*********************************************************************//**
+ * @brief       Check if the System Tick timer is currently enabled.
+ * @return      1 if enabled, 0 otherwise.
+ **********************************************************************/
+static __INLINE uint8_t SYSTICK_IsActive(void){
+    return (SysTick->CTRL & ST_CTRL_ENABLE) ? 1 : 0;
+}
+
+/*********************************************************************//**
+ * @brief       Check if the System Tick COUNTFLAG is set (timer has fired).
+ * @return      1 if COUNTFLAG is set, 0 otherwise.
+ **********************************************************************/
+static __INLINE uint8_t SYSTICK_HasFired(void) {
+    return (SysTick->CTRL & ST_CTRL_COUNTFLAG) ? 1 : 0;
+}
 
 /**
  * @}
